50ad555bdc6d370486335905dee84ecb07c8950d
Atul Baldaniya <atul.kanhasoft@gmail.com>
2022-01-12 11:32:46 +0530

    docker image not created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World this new push in master branch! v1 with pipeline v1');
+  res.send('Hello World this new push in master branch! v1 with pipeline v1 only push in server. docker image is not created');
 });
 
 var server = app.listen(3001, function () {
